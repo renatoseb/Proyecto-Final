3112afdf081560f81a1e29d94cccf74940925795
renatoseb <renatorodriguezllanos@gmail.com>
2019-06-27 23:37:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ raiz = Tk()
 
 raiz.title('Comparador')
 raiz.resizable(0,0)
-raiz.geometry('750x400')
+raiz.geometry('770x400')
 raiz.config(bg='gray')
+
+
+#-------------Parte de los archivos------------
 
 abrircoleccion = Button(raiz, text="Abrir colección")
 abrircoleccion.grid(column=2, row=0, padx=10, pady=10,sticky="w")
@@ -39,17 +42,29 @@ listatxt.insert(20,"webiwebin")
 listatxt.insert(21,"weasdsadn")
 listatxt.insert(22,"wesadsadn")
 scrolvert=Scrollbar(raiz, command=listatxt.yview)
-scrolvert.grid(row=2,column=4, sticky="nsew")
+scrolvert.grid(row=1,column=4, sticky="nsew")
 listatxt.config(yscrollcommand=scrolvert.set)
 
 
+
+#--------------Acá se mostrará la matriz a comparar impresa---------
+
 leerfigura= Button(raiz, text="Leer figura de consulta")
 leerfigura.grid(column=5, row=0, sticky="w", padx=10 )
-ventanamatriz= Listbox(raiz,height=20,width=35)
-ventanamatriz.grid(column=5, row=2,padx=10,columnspan=2,rowspan=2)
+ventanamatriz= Listbox(raiz,width=35)
+ventanamatriz.grid(column=5, row=1,padx=10,columnspan=2)
+
+
+#---------- Ranking -------------
 
 
 ranking= Button(raiz, text="Ranking de Similitud")
+ranking.grid(column=7, row= 0, sticky="w", padx=10)
+ventanaranking= Listbox(raiz,width=45,height=20)
+ventanaranking.grid(column=7, row=1, padx=10, columnspan=2)
+scrolito=Scrollbar(raiz, command=ventanaranking.yview)
+scrolito.grid(row=1,column=9, sticky="nsew")
+ventanaranking.config(yscrollcommand=scrolito.set)
 
 
 
