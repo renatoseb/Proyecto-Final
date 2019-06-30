c5c1f563cfb06661d4db72eff850e660fca8c1e1
Polomaru <52300915+Polomaru@users.noreply.github.com>
2019-06-29 20:31:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
 from tkinter import *
+def crear_ventana():
+    window = Toplevel(raiz)
+    window.geometry("300x300")
+    window.title("Nueva matriz")
+    milabel=Label(window,text="Ingrese nueva matriz")
+    milabel.pack()
+    entradamatriz=Entry(window)
+    entradamatriz.pack(fill=X , ipady=50)
+def abrir():
+    listatxt = Listbox(raiz,height=20,width=28)
+    listatxt.grid(column=2, row=1, padx=10,columnspan=2,rowspan=2,sticky="w")
+    for x in range(1,k+1):
+        listatxt.insert(x-1,"figura"+str(x)+".txt")
 
+    scrolvert=Scrollbar(raiz, command=listatxt.yview)
+    scrolvert.grid(row=1,column=4, sticky="nsew")
+    listatxt.config(yscrollcommand=scrolvert.set)
+
+#def leercoleccion():
+k=20
 raiz = Tk()
 
 raiz.title('Comparador')
 raiz.resizable(0,0)
-raiz.geometry('770x400')
+raiz.geometry('840x460')
 raiz.config(bg='gray')
-
+raiz.config(bd=35)
+raiz.config(relief="sunken")
 
 #-------------Parte de los archivos------------
 
-abrircoleccion = Button(raiz, text="Abrir colección")
+abrircoleccion = Button(raiz, text="Abrir colección",command=abrir)
 abrircoleccion.grid(column=2, row=0, padx=10, pady=10,sticky="w")
-
-agregar= Button(raiz, text="Agregar más...")
-agregar.grid(column=3, row=0 , pady=10)
-
 listatxt = Listbox(raiz,height=20,width=28)
 listatxt.grid(column=2, row=1, padx=10,columnspan=2,rowspan=2,sticky="w")
-listatxt.insert(0,"Holakace")
-listatxt.insert(1,"asdasf")
-listatxt.insert(2,"sadasf")
-listatxt.insert(3,"aeaeaeae")
-listatxt.insert(4,"puctaaa")
-listatxt.insert(5,"toyred")
-listatxt.insert(6,"nosomobica")
-listatxt.insert(7,"owowow")
-listatxt.insert(8,"webiwebin")
-listatxt.insert(9,"webiwebin")
-listatxt.insert(10,"webiwebin")
-listatxt.insert(11,"wefdsgsdfwebin")
-listatxt.insert(12,"wegfdsgn")
-listatxt.insert(13,"webihjfgjh")
-listatxt.insert(14,"wgfadsdsfn")
-listatxt.insert(15,"wdasfdsfn")
-listatxt.insert(16,"webgfsgsadin")
-listatxt.insert(17,"wegfdgsn")
-listatxt.insert(18,"webiwebin")
-listatxt.insert(19,"wegfdfgdn")
-listatxt.insert(20,"webiwebin")
-listatxt.insert(21,"weasdsadn")
-listatxt.insert(22,"wesadsadn")
-scrolvert=Scrollbar(raiz, command=listatxt.yview)
-scrolvert.grid(row=1,column=4, sticky="nsew")
-listatxt.config(yscrollcommand=scrolvert.set)
+
+
+agregar= Button(raiz, text="Agregar más...", command=crear_ventana)
+agregar.grid(column=3, row=0 , pady=10)
 
 
 
@@ -65,5 +59,6 @@ ventanaranking.grid(column=7, row=1, padx=10, columnspan=2)
 scrolito=Scrollbar(raiz, command=ventanaranking.yview)
 scrolito.grid(row=1,column=9, sticky="nsew")
 ventanaranking.config(yscrollcommand=scrolito.set)
+
 
 raiz.mainloop()
